3845898da105e67bee660e594b707b66b787f08f
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-11-20 23:01:25 -0400

    inicializacion repositorio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Products from "./components/Products"
+import ProductForm from "./components/ProductForm"
+
+const App = () => {
+  return (
+  <>
+    <ProductForm />
+    <Products />
+  </>
+  )
+}
+
+export default App
